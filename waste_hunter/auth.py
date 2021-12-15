b14034fb94f8f2a258a945bbd74c0cd3688fc1cc
bazzan <bazzanapalowo@gmail.com>
2021-12-15 11:12:33 +0100

    Database Revamp - New fields added to the database - Redirect to the '/' page

--- waste_hunter/auth.py
+++ waste_hunter/auth.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, request, flash, redirect
+from flask import Blueprint, url_for, render_template, request, flash, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
 from waste_hunter.db import User, db
 
@@ -9,27 +9,33 @@ bp = Blueprint('auth', __name__, url_prefix='')
 @bp.route("/signup", methods=('GET', 'POST'))
 def signup():
     if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
         email = request.form['email']
-        username = request.form['username']
+        phone_number = request.form['phone_number']
+        address = request.form['address']
         password = request.form['password']
 
         error = None
-        if not username:
-            error = 'Username is required'
+        if not email:
+            error = 'Email is required'
         elif not password:
             error = 'Password is required'
-        elif not email:
-            error = 'Email is required'
         elif User.query.filter_by(email=email).first():
             error = 'Email {} is already registered'.format(email)
-        elif User.query.filter_by(username=username).first():
-            error = "User {} is already registered".format(username)
 
         if error is None:
-            user = User(username=username, password=generate_password_hash(password), email=email)
+            user = User(
+                first_name=first_name,
+                last_name=last_name,
+                phone_number=phone_number,
+                address=address,
+                password=generate_password_hash(password),
+                email=email
+            )
             db.session.add(user)
             db.session.commit()
-            return redirect('index')
+            return redirect(url_for('index'))
 
         flash(error)
 
@@ -39,7 +45,7 @@ def signup():
 @bp.route("/login", methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['password']
 
--- waste_hunter/db.py
+++ waste_hunter/db.py
@@ -24,10 +24,13 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200))
+    last_name = db.Column(db.String(200))
+    address = db.Column(db.String(200))
+    phone_number = db.Column(db.String(20))
     email = db.Column(db.String(200), unique=True, nullable=False)
-    username = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __str__(self):
-        return self.username
+        return self.last_name + ' ' + self.first_name
